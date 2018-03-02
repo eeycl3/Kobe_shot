d392f5fa55ce00f384a0b498a99cd06523244eb5
eeycl3 <511395744@qq.com>
2018-03-02 14:27:23 -0500

    svm and clf updated

--- clf_randomForest.py
+++ clf_randomForest.py
@@ -1,16 +1,29 @@
 import pandas as pd
+import csv
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 trainingSet = pd.read_csv("trainingSet.csv")
 y = trainingSet["shot_made_flag"]
 
 # drop game_date_DT, secondsFromPeriodStart, secondsFromGameStart for avoiding overfit
-dropColumn = ['shot_made_flag','game_date_DT','secondsFromPeriodStart', 'secondsFromGameStart']
+dropColumn = ['shot_made_flag','game_date_DT','secondsFromPeriodStart', 'secondsFromGameStart','shot_id']
 for col in trainingSet:
     if 'ShotType_' in col:
         dropColumn.append(col)
 trainingSet.drop(columns=dropColumn, axis=1, inplace=True)
-clf = RandomForestClassifier(n_jobs=2, random_state=0, oob_score= True, n_estimators= 100, max_features="auto")
+clf = RandomForestClassifier(n_jobs=2, random_state=0, n_estimators= 15, max_features="auto", max_depth=10)
 clf.fit(trainingSet, y)
+predictSet = pd.read_csv("predictSet.csv")
+yy = predictSet["shot_made_flag"]
+id = predictSet["shot_id"]
+predictSet.drop(dropColumn, axis=1, inplace=True)
+result = clf.predict_proba(predictSet)[:,1]
 scores = cross_val_score(clf, trainingSet, y)
 print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+file1 = open("RF_result.csv",'w',newline='')    # goal
+writer1 = csv.writer(file1)
+writer1.writerow(["shot_id", "shot_made_flag"])
+for index in range(len(result)):
+    writer1.writerow([id[index], result[index]])
+
--- clf_svm.py
+++ clf_svm.py
@@ -2,11 +2,12 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 
 trainingSet = pd.read_csv('trainingSet.csv', sep=',')
 y = trainingSet["shot_made_flag"]
 
-trainingSet = trainingSet.drop(['game_date_DT', 'secondsFromPeriodStart'], axis=1)
+trainingSet.drop(['game_date_DT', 'secondsFromPeriodStart', 'shot_made_flag','shot_id'], axis=1, inplace=True)
 
 # print(data)
 
@@ -14,9 +15,13 @@ scaler = StandardScaler(copy=True, with_mean=True, with_std=True)
 scaler.fit(trainingSet)
 data_norm = scaler.transform(trainingSet)
 
-x_train, x_test, y_train, y_test = train_test_split(trainingSet, y, test_size=0.3)
+x_train, x_test, y_train, y_test = train_test_split(data_norm, y, test_size=0.3, random_state=42)
 
-svm = SVC()
+#svm = SVC()
+#svm = SVC(kernel='linear')
+svm = LinearSVC()
 svm.fit(x_train, y_train)
+score_svm_train = svm.score(x_train, y_train)
 score_svm = svm.score(x_test, y_test)
+print(score_svm_train)
 print(score_svm)
