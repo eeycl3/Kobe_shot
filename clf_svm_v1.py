5ae170e54a5d56fae8d72da6d8a028ded4486be4
Zizheng JIANG <jiangzizheng1205@gmail.com>
2018-03-15 16:27:55 -0400

    svm version1

--- clf_svm_v1.py
+++ clf_svm_v1.py
@@ -14,7 +14,7 @@ dropColumn = ['game_date_DT', 'secondsFromPeriodStart', 'shot_made_flag', 'shot_
 trainingSet.drop(dropColumn, axis=1, inplace=True)
 
 
-X_train, X_test, y_train, y_test = train_test_split(data_norm, y, test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(trainingSet, y, test_size=0.33, random_state=42)
 
 scaler = StandardScaler(copy=True, with_mean=True, with_std=True)
 scaler.fit(X_train)
@@ -25,9 +25,7 @@ max_score, max_c, max_gamma = 0, 0, 0
 c = [0.1, 0.5, 1, 2, 5, 10, 20, 50]
 gamma = [0.01, 0.05, 0.1, 0.5, 1, 2, 5, 10]
 for i in c:
-    print("Hello from loop 1")
     for j in gamma:
-        print("Hello from loop 2")
         clf_svm = SVC(C = i, gamma=j)
         scores = cross_val_score(clf_svm, X_train_norm, y_train, cv=5)
         score = scores.mean()
