f5e50a01486a1165d0b3a81fad8d3eb09c175eb2
eeycl3 <511395744@qq.com>
2018-03-15 21:04:29 -0400

    clf_MLP.py

--- clf_MLP.py
+++ clf_MLP.py
@@ -1,34 +1,41 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+import matplotlib.pyplot as plt
 
+X_train = pd.read_csv('X_train_rfe_20.csv', sep=',')
+y = pd.read_csv('y_train_rfe_20.csv', sep = ',')
+y_train = y["shot_made_flag"]
 
-trainingSet = pd.read_csv('trainingSet.csv', sep=',')
-y = trainingSet["shot_made_flag"]
-dropColumn = ['game_date_DT', 'secondsFromPeriodStart', 'shot_made_flag', 'shot_id']
-trainingSet.drop(dropColumn, axis=1, inplace=True)
-
-
-X_train, X_test, y_train, y_test = train_test_split(trainingSet, y, test_size=0.33, random_state=42)
-
-scaler = StandardScaler(copy=True, with_mean=True, with_std=True)
-X_train_norm = scaler.fit_transform(X_train)
-X_test_norm = scaler.transform(X_test)
-
-max_test_score = 0
-for i in range(1, 200):
-    for j in range(1, 200):
-        clf_mlp = MLPClassifier(solver='sgd', hidden_layer_sizes=(i, j), random_state=0, activation='tanh')
+max_score = 0
+min_score_std = 999999999999999999999999
+param = 50
+matrix_mean_score = [[0] * param] * param
+matrix_score_std = [[0] * param] * param
+for i in range(1, param):
+    for j in range(1, param):
+        clf_mlp = MLPClassifier(solver='lbfgs', hidden_layer_sizes=(i, j), random_state=0, activation='tanh')
         clf_mlp.fit(X_train, y_train)
-        train_score = clf_mlp.score(X_train, y_train)
-        test_score = clf_mlp.score(X_test, y_test)
-        if test_score > max_test_score:
-            related_train_score = train_score
-            max_test_score = test_score
+        scores = cross_val_score(clf_mlp, X_train, y_train)
+        matrix_mean_score[i][j] = scores.mean()
+        matrix_score_std[i][j] = scores.std()
+        if scores.mean() > max_score:
+            max_score = scores.mean()
+            min_score_std = scores.std()
             max_i = i
             max_j = j
             best_clf_mlp = clf_mlp
 
-print("2 hidden layer",max_i, max_j, "train score: ", related_train_score, "test score: ", max_test_score)
+plt.figure()
+im=plt.imshow(matrix_mean_score)
+plt.title("MLP accuracy score with different units in 2 hidden layer")
+plt.colorbar(im)
+plt.xlim(1, param)
+plt.ylim(1, param)
+plt.xlabel("first hidden layer unit number")
+plt.ylabel("second hidden layer unit number")
+plt.savefig("MLP_param_figure.png")
+print("2 hidden layer",max_i, max_j,"score: ", max_score, "std: ", min_score_std)
+print(matrix_mean_score)
+print(matrix_score_std)
 
