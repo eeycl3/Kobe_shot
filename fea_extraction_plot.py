54f64f6bc508d0a2c5f7a2c6081b2fb966b2193e
eeycl3 <511395744@qq.com>
2018-03-22 14:38:35 -0400

    TSNE update

--- fea_extraction_plot.py
+++ fea_extraction_plot.py
@@ -12,7 +12,7 @@ X_test = pd.read_csv('X_test_rfe_20.csv', sep=',')
 y_train = pd.read_csv('y_train.csv', sep=',')
 y_test = pd.read_csv('y_test.csv', sep=',')
 y_train= y_train["shot_made_flag"]
-
+classLable = ["shot missed", "shot made"]
 colors=["r", "b"]
 """
 pca = PCA(n_components=2, svd_solver="full")
@@ -36,6 +36,9 @@ for l, color in zip(np.unique(y_train), colors):
 tsne = TSNE(n_components=2)
 tsne_X_train = tsne.fit_transform(X_train)
 print(len(tsne_X_train))
-for l, color in zip(np.unique(y_train), colors):
-    plt.scatter(tsne_X_train[y_train==l, 0], tsne_X_train[y_train==l, 1],  c = color)
+for l, color,name in zip(np.unique(y_train), colors, classLable):
+    plt.scatter(tsne_X_train[y_train==l, 0], tsne_X_train[y_train==l, 1], c = color, label = name )
+plt.title("TSNE reduction for visualization on training data")
+plt.xlabel("TSNE component 1")
+plt.ylabel("TSNE component 2")
 plt.show()
