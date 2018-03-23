58b3a2ca73ea96d157f71fdf032b0a6f0ca2b014
eeycl3 <511395744@qq.com>
2018-03-22 22:06:33 -0400

    graphs updated

--- plotting scripts/fea_extraction_plot.py
+++ plotting scripts/fea_extraction_plot.py
@@ -8,7 +8,7 @@ from sklearn.manifold import TSNE
 
 
 X_train = pd.read_csv('X_train_rfe_40.csv', sep=',')
-X_test = pd.read_csv('X_test_rfe_40.csv', sep=',')
+X_test = pd.read_csv('X_test_rfe_20.csv', sep=',')
 y_train = pd.read_csv('y_train.csv', sep=',')
 y_test = pd.read_csv('y_test.csv', sep=',')
 y_train= y_train["shot_made_flag"]
@@ -38,6 +38,7 @@ tsne_X_train = tsne.fit_transform(X_train)
 print(len(tsne_X_train))
 for l, color,name in zip(np.unique(y_train), colors, classLable):
     plt.scatter(tsne_X_train[y_train==l, 0], tsne_X_train[y_train==l, 1], c = color, label = name )
+plt.legend()
 plt.title("TSNE reduction for visualization on training data")
 plt.xlabel("TSNE component 1")
 plt.ylabel("TSNE component 2")
